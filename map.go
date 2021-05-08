a29b2cb26c59f02206e7bc5a4149c5f987888b9f
Andy Smith <andy.smith@90poe.io>
2021-05-08 15:39:11 +0100

    Add a simple sorted map

---
+++ map.go
@@ -0,0 +1,105 @@
+package lsm
+
+import (
+	"sort"
+)
+
+type (
+	Map struct {
+		items map[string]interface{}
+		order []string
+	}
+)
+
+// insertMap inserts the key and val into the map and returns
+// true if  a new key was inserted
+func (m *Map) insertMap(key string, val interface{}) bool {
+	if m.items == nil {
+		m.items = map[string]interface{}{}
+	}
+	_, ok := m.items[key]
+	m.items[key] = val
+	return !ok
+}
+
+// insertSlice inserts the key into the slice and returns
+// true if a new key was inserted
+func (m *Map) insertSlice(key string) bool {
+	i := sort.SearchStrings(m.order, key)
+	if i == len(m.order) {
+		m.order = append(m.order, key)
+		return true
+	}
+	if key == m.order[i] {
+		return false
+	}
+	m.order = append(m.order[:i+1], m.order[i:]...)
+	m.order[i] = key
+	return true
+}
+
+// deleteMap deletes the key from the the map and returns
+// true if the key was deleted
+func (m *Map) deleteMap(key string) bool {
+	_, ok := m.items[key]
+	delete(m.items, key)
+	return ok
+}
+
+// deleteSlice deletes the key from the slice and returns
+// true if it was deleted
+func (m *Map) deleteSlice(key string) bool {
+	i := sort.SearchStrings(m.order, key)
+	if i == len(m.order) {
+		return false
+	}
+	if m.order[i] != key {
+		return false
+	}
+	m.order = append(m.order[:i], m.order[i+1:]...)
+	return true
+}
+
+func (m *Map) Add(key string, val interface{}) {
+	ok := m.insertMap(key, val)
+	if ok {
+		ok = m.insertSlice(key)
+		if !ok {
+			panic("should never occur")
+		}
+	}
+}
+
+func (m *Map) Remove(key string) {
+	ok := m.deleteMap(key)
+	if ok {
+		ok := m.deleteSlice(key)
+		if !ok {
+			panic("should never occur")
+		}
+	}
+}
+
+func (m *Map) Get(key string) (interface{}, bool) {
+	val, ok := m.items[key]
+	return val, ok
+}
+
+// Iter returns a function that can be used to iterate through the map
+// in key-sorted order. The key parameter specifies the start position.
+func (m *Map) Iter(key string) func() (string, interface{}, bool) {
+	var i int
+	if key != "" {
+		i = sort.SearchStrings(m.order, key)
+	}
+
+	return func() (string, interface{}, bool) {
+		if i < len(m.order) {
+			k := m.order[i]
+			v := m.items[k]
+			i++
+			return k, v, true
+		}
+		return "", nil, false
+	}
+}
---
+++ map_test.go
@@ -0,0 +1,111 @@
+package lsm
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMap_AddRemove(t *testing.T) {
+	var m Map
+
+	m.Add("x", 1)
+	invariants(t, m)
+	val, ok := m.Get("x")
+	require.True(t, ok)
+	require.Equal(t, 1, val)
+
+	m.Add("y", 2)
+	invariants(t, m)
+	val, ok = m.Get("y")
+	require.True(t, ok)
+	require.Equal(t, 2, val)
+
+	m.Remove("x")
+	invariants(t, m)
+	val, ok = m.Get("x")
+	require.False(t, ok)
+	require.Nil(t, val)
+
+	val, ok = m.Get("y")
+	invariants(t, m)
+	require.True(t, ok)
+	require.Equal(t, 2, val)
+
+	m.Remove("y")
+	invariants(t, m)
+	val, ok = m.Get("x")
+	require.False(t, ok)
+	require.Nil(t, val)
+
+	val, ok = m.Get("y")
+	invariants(t, m)
+	require.False(t, ok)
+	require.Nil(t, val)
+
+	m.Remove("z")
+}
+
+func TestMap_Iter(t *testing.T) {
+	var m Map
+	m.Add("z", 3)
+	m.Add("w", 1)
+	m.Add("y", 2)
+
+	f := func(it func() (string, interface{}, bool)) ([]string, []interface{}) {
+		var keys []string
+		var vals []interface{}
+
+		for {
+			key, val, ok := it()
+			if !ok {
+				break
+			}
+			keys = append(keys, key)
+			vals = append(vals, val)
+		}
+		return keys, vals
+	}
+
+	keys, vals := f(m.Iter(""))
+	invariants(t, m)
+	require.Equal(t, []string{"w", "y", "z"}, keys)
+	require.Equal(t, []interface{}{1, 2, 3}, vals)
+
+	keys, vals = f(m.Iter("v"))
+	invariants(t, m)
+	require.Equal(t, []string{"w", "y", "z"}, keys)
+	require.Equal(t, []interface{}{1, 2, 3}, vals)
+
+	keys, vals = f(m.Iter("w"))
+	invariants(t, m)
+	require.Equal(t, []string{"w", "y", "z"}, keys)
+	require.Equal(t, []interface{}{1, 2, 3}, vals)
+
+	keys, vals = f(m.Iter("x"))
+	invariants(t, m)
+	require.Equal(t, []string{"y", "z"}, keys)
+	require.Equal(t, []interface{}{2, 3}, vals)
+
+	keys, vals = f(m.Iter("y"))
+	invariants(t, m)
+	require.Equal(t, []string{"y", "z"}, keys)
+	require.Equal(t, []interface{}{2, 3}, vals)
+
+	keys, vals = f(m.Iter("z"))
+	invariants(t, m)
+	require.Equal(t, []string{"z"}, keys)
+	require.Equal(t, []interface{}{3}, vals)
+
+	keys, vals = f(m.Iter("{"))
+	invariants(t, m)
+	require.Nil(t, keys)
+	require.Nil(t, vals)
+}
+
+func invariants(t *testing.T, m Map) {
+	require.Equal(t, len(m.items), len(m.order))
+	for _, key := range m.order {
+		_, ok := m.items[key]
+		require.True(t, ok)
+	}
+}
